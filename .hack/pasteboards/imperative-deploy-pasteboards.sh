300e683374a445780b8a251ec98400ff0edfd779
Jason Dudash <jason.dudash@gmail.com>
2023-04-27 17:11:28 -0400

    update deploymentconfigs to deployments

--- .hack/pasteboards/imperative-deploy-pasteboards.sh
+++ .hack/pasteboards/imperative-deploy-pasteboards.sh
@@ -6,7 +6,7 @@ oc -n openshift tag registry.redhat.io/rhscl/nodejs-10-rhel7 nodejs:10
 
 # We are just going to manually run through the steps outlined in the workshop to get things ready to showcase service mesh
 # https://github.com/RedHatGov/service-mesh-workshop-dashboard/blob/main/workshop/content/lab1.3_deploymsa.md
-oc new-app -f ./config/app/boards-fromsource.yaml \
+oc new-app template-boards-fromsource \
   -p APPLICATION_NAME=boards \
   -p NODEJS_VERSION_TAG=16-ubi8 \
   -p GIT_URI=https://github.com/RedHatGov/service-mesh-workshop-code.git \
@@ -14,21 +14,30 @@ oc new-app -f ./config/app/boards-fromsource.yaml \
   -p DATABASE_SERVICE_NAME=boards-mongodb \
   -p MONGODB_DATABASE=boardsDevelopment
 
-oc new-app -f ./config/app/context-scraper-fromsource.yaml \
+oc new-app template-context-scraper-fromsource \
   -p APPLICATION_NAME=context-scraper \
   -p NODEJS_VERSION_TAG=16-ubi8 \
   -p GIT_BRANCH=workshop-stable \
   -p GIT_URI=https://github.com/RedHatGov/service-mesh-workshop-code.git
 
-oc new-app -f ./config/app/app-ui-fromsource.yaml \
+oc new-app template-app-ui-fromsource \
   -p APPLICATION_NAME=app-ui \
   -p NODEJS_VERSION_TAG=16-ubi8 \
   -p GIT_BRANCH=workshop-stable \
   -p GIT_URI=https://github.com/RedHatGov/service-mesh-workshop-code.git \
   -e FAKE_USER=true
 
+oc new-app template-userprofile-build \
+  -p APPLICATION_NAME=userprofile \
+  -p APPLICATION_CODE_URI=https://github.com/RedHatGov/service-mesh-workshop-code.git \
+  -p APPLICATION_CODE_BRANCH=workshop-stable \
+  -p APP_VERSION_TAG=1.0
+oc start-build userprofile-1.0
+
 oc get pods
 
 oc create -f ./istio/gateway.yaml
+
+echo The istio ingress gateway is:
 GATEWAY_URL=$(oc get route istio-ingressgateway -n istio-system --template='http://{{.spec.host}}')
 echo $GATEWAY_URL
